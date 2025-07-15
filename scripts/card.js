16b597cf107efcb2a1e51da959a344ace5fcf51b
shnfi <theshnfi@gmail.com>
2025-07-15 17:10:11 +0330

    fixed the 'double event listener' bug by modifying the 'deal' function

--- scripts/card.js
+++ scripts/card.js
@@ -11,7 +11,7 @@ var cards_on_hand = [];
 
 var ans = 0;
 
-function deal() {
+function deal(first_round) {
     for (let i = 0; i < cards_arr.length; i++) {
         /* adding the event listener to each card using this loop, for better performance and lower memory user */
 
@@ -24,30 +24,32 @@ function deal() {
             cards_arr[i].classList.add("off");
         }
 
-        console.log(`class list of the ${i+1}th card: ${cards_arr[i].classList}`); // debug line, remove me later
-
         /* adding the event listener for subtraction and addition for applying */
 
-        cards_arr[i].addEventListener("click", () => {
-            if (cards_arr[i].classList[2] == "off") {
-                cards_arr[i].classList.toggle("off");
-                cards_arr[i].classList.toggle("on");
-                cards_arr[i].style.translate = "0 -3rem";
-
-                ans += cards_on_hand[cards_arr[i].classList[1]];
-            }
-            else if (cards_arr[i].classList[2] == "on") {
-                cards_arr[i].classList.toggle("on");
-                cards_arr[i].classList.toggle("off");
-                cards_arr[i].style.translate = "0 0";
-
-                ans -= cards_on_hand[cards_arr[i].classList[1]];
-            }
-
-            console.log(`class list of clicked card: ${cards_arr[i].classList}`); // debug line, remove me later
-            console.log(`ans = ${ans}`); // debug line, remove me later
-            console.log("--------------------------"); // debug line, remove me later
-        })
+        if (first_round) {
+            cards_arr[i].addEventListener("click", () => {
+                if (cards_arr[i].classList[2] == "off") {
+                    cards_arr[i].classList.toggle("off");
+                    cards_arr[i].classList.toggle("on");
+    
+                    cards_arr[i].style.translate = "0 -3rem";
+    
+                    ans += cards_on_hand[cards_arr[i].classList[1]];
+                }
+                else if (cards_arr[i].classList[2] == "on") {
+                    cards_arr[i].classList.toggle("on");
+                    cards_arr[i].classList.toggle("off");
+    
+                    cards_arr[i].style.translate = "0 0";
+    
+                    ans -= cards_on_hand[cards_arr[i].classList[1]];
+                }
+    
+                console.log(`class list of clicked card: ${cards_arr[i].classList}`); // debug line, remove me later
+                console.log(`ans = ${ans}`); // debug line, remove me later
+                console.log("--------------------------"); // debug line, remove me later
+            })
+        }
 
         const num = Math.floor(Math.random() * 10 + 1);
         cards_arr[i].style.backgroundImage = `url(assets/${num}_of_${possible_cards[Math.floor(Math.random() * possible_cards.length)]}.png)`;
@@ -57,4 +59,4 @@ function deal() {
     console.log(cards_on_hand); // debug line, remove me later
 }
 
-deal();
+deal(true);
