dff72554fd0036387bf7da6f930a1b590df42ca2
shnfi <theshnfi@gmail.com>
2025-07-15 17:10:38 +0330

    just wrote the 'next round' function

--- scripts/logic.js
+++ scripts/logic.js
@@ -5,15 +5,34 @@ const win_circles = document.querySelectorAll(".circle");
 
 var win_streak = 0;
 
+function next_round() {
+	cards_on_hand.length = 0;
+	ans = 0;
+
+	randomize();
+	deal(false);
+
+	for (let i = 0; i < cards_arr.length; i++) {
+		if (cards_arr[i].classList[2] == "on") {
+			cards_arr[i].classList.toggle("on");
+			cards_arr[i].classList.toggle("off");
+		}
+		
+		cards_arr[i].style.translate = "0 0";
+	}
+}
+
 function win() {
     win_streak++
     win_circles[win_streak - 1].style.backgroundColor = "lightGreen";
 
     if (win_streak == 3) {
 		setTimeout(() => {
-		        alert("You won nigga!");
+		    alert("You won nigga!");
 		}, 1500);
 	}
+
+	next_round();
 }
 
 apply_btn.addEventListener("click", () => {
